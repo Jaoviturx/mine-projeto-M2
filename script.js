caaa439b291c5340a95c4c2c1e0778926a691389
Jaoviturx <joaovitorcruci555@gmail.com>
2025-04-04 17:32:18 -0300

    Fezendo javascript

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
+const form = document.getElementById("form-cadastro");
+const listafilmes = document.getElementById("lista-filmes");
+
+// Função de adicionar filmes
+function adicionarfilme(event) {
+    event.preventDefault();
+
+    // Pega os valores
+    const titulo = document.getElementById("titulo").value;
+    const ano = document.getElementById("ano").value;
+    const genero = document.getElementById("genero").value;
+    const descriçao = document.getElementById("descriçao").value;
+
+    // Cria item da lista
+    const li = document.createElement("li");
+    li.innerHTML = `<h3>${titulo} (${ano})</h3><p><strong>Gênero:</strong> ${genero}</p><p><strong>Descrição:</strong> ${descriçao}</p> 
+    <button id="editar">editar</button>
+        <button id="deletar">deletar</button>`;
+
+    // Adiciona o item na lista
+    listafilmes.appendChild(li);
+
+    // Limpa os campos do formulário após adicionar
+    form.reset();
+}
+//adiciona o evento de submit no formulario 
+form.addEventListener("submit", adicionarfilme);
 
 
